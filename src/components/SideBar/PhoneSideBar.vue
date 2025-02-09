0ff770ac253d1e8ce8dc9accdee0351b880bf2c4
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2025-02-09 09:32:35 +0800

    pref: dark mode side bar color

--- src/components/SideBar/PhoneSideBar.vue
+++ src/components/SideBar/PhoneSideBar.vue
@@ -19,7 +19,7 @@ const windowWidthStore = useWindowWidthStore()
     :class="`fixed h-screen w-screen ${open ? 'left-0' : 'left-[-100%]'} transition-all duration-300`"
     @click.stop="emit('openFn')"
   >
-    <div class="h-full w-[70vw] backdrop-blur-md bg-gray-300/70 shadow-xl rounded-tr-2xl rounded-br-2xl">
+    <div class="h-full w-[70vw] backdrop-blur-md bg-gray-300/70 dark:bg-gray-800/70 shadow-xl rounded-tr-2xl rounded-br-2xl">
       <div class="pl-6 pt-4" @click.stop="emit('openFn')">
         <span class="material-icons dark:text-white/70 mr-4"> menu </span>
       </div>
